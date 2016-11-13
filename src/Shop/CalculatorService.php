e8f8ee55a060d9acd57c629af9284efb95137e9d
Hauke Stange <partyschaum@posteo.de>
2016-11-13 13:25:52 +0100

    Fix complexity caused by state
    Remove state from CalculatorService class.

--- src/Shop/CalculatorService.php
+++ src/Shop/CalculatorService.php
@@ -4,9 +4,6 @@ namespace DDDHH\Shop;
 
 class CalculatorService
 {
-    /** @var bool */
-    private $discountActive = false;
-
     /**
      * @param ShoppingCart $cart
      * @param float $discount
@@ -16,15 +13,17 @@ class CalculatorService
     public function total(ShoppingCart $cart, float $discount = 1.0, array $discountedItemIds = []): float
     {
         $total = 0.0;
+        $discountActive = false;
+
         foreach ($cart->items() as $item) {
             $total += $item->quantity() * $item->pricePerUnit();
 
             if (in_array($item->id(), $discountedItemIds)) {
-                $this->discountActive = true;
+                $discountActive = true;
             }
         }
 
-        if ($this->discountActive) {
+        if ($discountActive) {
             $total *= $discount;
         }
 
--- tests/Shop/CalculatorServiceTest.php
+++ tests/Shop/CalculatorServiceTest.php
@@ -69,4 +69,51 @@ class CalculatorServiceTest extends TestCase
             $discountedTotal
         );
     }
+
+    /**
+     * @test
+     */
+    public function itShouldConsiderDiscountsCorrectly()
+    {
+        $calc = new CalculatorService();
+
+        $cart = new ShoppingCart();
+
+        $discountedItem = new Item(
+            'AAXX-4711',
+            'Working Effectively with Legacy Code',
+            47.95,
+            1
+        );
+
+        $cart->addItem($discountedItem);
+        $cart->addItem(new Item(
+            'BBZZ-0815',
+            'Domain-Driven Design: Tackling Complexity in the Heart of Software',
+            54.95,
+            2
+        ));
+
+        $discountedItemIds = [ $discountedItem->id() ];
+        $discount = 0.9;
+        $discountedTotal = $calc->total($cart, $discount, $discountedItemIds);
+
+        $expectedTotal = (1 * 47.95 + 2 * 54.95) * $discount;
+
+        $this->assertEquals(
+            $expectedTotal,
+            $discountedTotal
+        );
+
+        $cart->removeItem($discountedItem);
+
+        $discountedTotal = $calc->total($cart, $discount, $discountedItemIds);
+
+        $expectedTotal = 2 * 54.95;
+
+        $this->assertEquals(
+            $expectedTotal,
+            $discountedTotal
+        );
+    }
 }
