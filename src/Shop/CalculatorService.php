66bd01c95dcf5de63a5b46450d621431ff2b5a47
Hauke Stange <hauke@jimdo.com>
2016-11-14 13:10:40 +0100

    Extract CalculatorService interface
    Move code from former CalculatorService into
    ImperativeCalculatorService.

--- src/Shop/CalculatorService.php
+++ src/Shop/CalculatorService.php
@@ -2,32 +2,13 @@
 
 namespace DDDHH\Shop;
 
-class CalculatorService
+interface CalculatorService
 {
-    /** @var bool */
-    private $discountActive = false;
-
     /**
      * @param ShoppingCart $cart
      * @param float $discount
      * @param string[] $discountedItemIds
      * @return float
      */
-    public function total(ShoppingCart $cart, float $discount = 1.0, array $discountedItemIds = []): float
-    {
-        $total = 0.0;
-        foreach ($cart->items() as $item) {
-            $total += $item->quantity() * $item->pricePerUnit();
-
-            if (in_array($item->id(), $discountedItemIds)) {
-                $this->discountActive = true;
-            }
-        }
-
-        if ($this->discountActive) {
-            $total *= $discount;
-        }
-
-        return $total;
-    }
+    public function total(ShoppingCart $cart, float $discount = 1.0, array $discountedItemIds = []): float;
 }
